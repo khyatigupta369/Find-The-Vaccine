9817883c3e97327f4e7ad92543c116d572632caa
Unnati <unnat4ucreative@gmail.com>
2020-10-22 21:58:45 +0530

    Answers.js add

---
+++ public/answers.js
@@ -0,0 +1,211 @@
+function changePopulation1() {
+  var ans1 = document.querySelector(".ans1")
+  var pop1 = document.getElementById("pop1").value;
+
+  if (ans1.value === "4") {
+    var x = pop1 / 2;
+    document.getElementById("pop2").value = x;
+    console.log("population: " + document.getElementById("pop2").value);
+  } else {
+    var y = pop1 * 2;
+    document.getElementById("pop2").value = y;
+    console.log("population: " + document.getElementById("pop2").value);
+  }
+}
+function changePopulation2(){
+  var ans2 = document.querySelector(".ans2")
+  var pop1 = document.getElementById("pop1").value;
+
+  if (ans2.value === "5") {
+    var x = pop1 / 2;
+    document.getElementById("pop2").value = x;
+    console.log("population: " + document.getElementById("pop2").value);
+  }
+  else {
+    var y = pop1 * 2;
+    document.getElementById("pop2").value = y;
+    console.log("population: " + document.getElementById("pop2").value);
+  }
+}
+function changePopulation3(){
+  var ans3 = document.querySelector(".ans3")
+  var pop1 = document.getElementById("pop1").value;
+
+  if (ans3.value === "6") {
+    var x = pop1 / 2;
+    document.getElementById("pop2").value = x;
+    console.log("population: " + document.getElementById("pop2").value);
+  }
+  else {
+    var y = pop1 * 2;
+    document.getElementById("pop2").value = y;
+    console.log("population: " + document.getElementById("pop2").value);
+  }
+}
+function changePopulation4(){
+  var ans4 = document.querySelector(".ans4")
+  var pop1 = document.getElementById("pop1").value;
+
+  if (ans4.value === "7") {
+    var x = pop1 / 2;
+    document.getElementById("pop2").value = x;
+    console.log("population: " + document.getElementById("pop2").value);
+  }
+  else {
+    var y = pop1 * 2;
+    document.getElementById("pop2").value = y;
+    console.log("population: " + document.getElementById("pop2").value);
+  }
+
+}
+function changePopulation5(){
+  var ans5 = document.querySelector(".ans5")
+  var pop1 = document.getElementById("pop1").value;
+
+  if (ans5.value === "8") {
+    var x = pop1 / 2;
+    document.getElementById("pop2").value = x;
+    console.log("population: " + document.getElementById("pop2").value);
+  }
+  else {
+    var y = pop1 * 2;
+    document.getElementById("pop2").value = y;
+    console.log("population: " + document.getElementById("pop2").value);
+  }
+
+}
+function changePopulation6(){
+  var con1 = document.querySelector(".con1")
+  var pop1 = document.getElementById("pop1").value;
+
+  if (con1.value === "9") {
+    var x = pop1 / 2;
+    document.getElementById("pop2").value = x;
+  }
+  else {
+    var y = pop1 * 2;
+    document.getElementById("pop2").value = y;
+  }
+
+}
+function changePopulation7(){
+  var con2 = document.querySelector(".con2")
+  var pop1 = document.getElementById("pop1").value;
+
+  if (con2.value === "10") {
+    var x = pop1 / 2;
+    document.getElementById("pop2").value = x;
+  }
+  else {
+    var y = pop1 * 2;
+    document.getElementById("pop2").value = y;
+  }
+
+}
+function changePopulation8(){
+  var con3 = document.querySelector(".con3")
+  var pop1 = document.getElementById("pop1").value;
+
+  if (con3.value === "11") {
+    var x = pop1 / 2;
+    document.getElementById("pop2").value = x;
+  }
+  else {
+    var y = pop1 * 2;
+    document.getElementById("pop2").value = y;
+  }
+
+}
+function changePopulation9(){
+  var con4 = document.querySelector(".con4")
+  var pop1 = document.getElementById("pop1").value;
+
+  if (con4.value === "12") {
+    var x = pop1 / 2;
+    document.getElementById("pop2").value = x;
+  }
+  else {
+    var y = pop1 * 2;
+    document.getElementById("pop2").value = y;
+  }
+
+}
+function changePopulation10(){
+  var con5 = document.querySelector(".con5")
+  var pop1 = document.getElementById("pop1").value;
+
+  if (con5.value === "13") {
+    var x = pop1 / 2;
+    document.getElementById("pop2").value = x;
+  }
+  else {
+    var y = pop1 * 2;
+    document.getElementById("pop2").value = y;
+  }
+
+}
+function changePopulation11() {
+  var ans11 = document.querySelector(".ans11")
+  var pop1 = document.getElementById("pop1").value;
+
+  if (ans11.value === "2") {
+    var x = pop1 / 2;
+    document.getElementById("pop2").value = x;
+  } else {
+    var y = pop1 * 2;
+    document.getElementById("pop2").value = y;
+  }
+
+}
+function changePopulation12() {
+  var ans12 = document.querySelector(".ans12")
+  var pop1 = document.getElementById("pop1").value;
+
+  if (ans12.value === "6") {
+    var x = pop1 / 2;
+    document.getElementById("pop2").value = x;
+  } else {
+    var y = pop1 * 2;
+    document.getElementById("pop2").value = y;
+  }
+
+}
+function changePopulation13() {
+  var ans13 = document.querySelector(".ans13")
+  var pop1 = document.getElementById("pop1").value;
+
+  if (ans13.value === "8") {
+    var x = pop1 / 2;
+    document.getElementById("pop2").value = x;
+  } else {
+    var y = pop1 * 2;
+    document.getElementById("pop2").value = y;
+  }
+
+}
+function changePopulation14() {
+  var ans14 = document.querySelector(".ans14")
+  var pop1 = document.getElementById("pop1").value;
+
+  if (ans14.value === "10") {
+    var x = pop1 / 2;
+    document.getElementById("pop2").value = x;
+  } else {
+    var y = pop1 * 2;
+    document.getElementById("pop2").value = y;
+  }
+
+}
+function changePopulation15() {
+  var ans15 = document.querySelector(".ans15")
+  var pop1 = document.getElementById("pop1").value;
+
+  if (ans15.value === "12") {
+    var x = pop1 / 2;
+    document.getElementById("pop2").value = x;
+  } else {
+    var y = pop1 * 2;
+    document.getElementById("pop2").value = y;
+  }
+
+}
